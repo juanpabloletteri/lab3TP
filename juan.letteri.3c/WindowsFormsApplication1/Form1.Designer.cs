bb39f1fb3c1226213066ba29f7691a4e8bc0d16e
juanpabloletteri <pabloletteri@gmail.com>
2016-09-17 16:10:07 -0300

    combo box pruebas

---
+++ juan.letteri.3c/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lblItem = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.cmb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.cmbColores = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(224, 310);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lblItem
+            // 
+            this.lblItem.AutoSize = true;
+            this.lblItem.Location = new System.Drawing.Point(387, 87);
+            this.lblItem.Name = "lblItem";
+            this.lblItem.Size = new System.Drawing.Size(35, 13);
+            this.lblItem.TabIndex = 1;
+            this.lblItem.Text = "label1";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(387, 119);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(35, 13);
+            this.lblText.TabIndex = 2;
+            this.lblText.Text = "label2";
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Location = new System.Drawing.Point(387, 153);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(35, 13);
+            this.lblIndex.TabIndex = 3;
+            this.lblIndex.Text = "label3";
+            // 
+            // cmb
+            // 
+            this.cmb.FormattingEnabled = true;
+            this.cmb.Items.AddRange(new object[] {
+            "hola",
+            "chau",
+            "tal vez",
+            "amarillo",
+            "azul",
+            "violeta"});
+            this.cmb.Location = new System.Drawing.Point(45, 102);
+            this.cmb.Name = "cmb";
+            this.cmb.Size = new System.Drawing.Size(121, 21);
+            this.cmb.TabIndex = 4;
+            this.cmb.SelectedIndexChanged += new System.EventHandler(this.cmb_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "index";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "item";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(323, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "value";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(387, 189);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(35, 13);
+            this.lblValue.TabIndex = 8;
+            this.lblValue.Text = "label3";
+            // 
+            // cmbColores
+            // 
+            this.cmbColores.FormattingEnabled = true;
+            this.cmbColores.Location = new System.Drawing.Point(45, 181);
+            this.cmbColores.Name = "cmbColores";
+            this.cmbColores.Size = new System.Drawing.Size(121, 21);
+            this.cmbColores.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 383);
+            this.Controls.Add(this.cmbColores);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmb);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.lblItem);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label lblItem;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblIndex;
+        private System.Windows.Forms.ComboBox cmb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.ComboBox cmbColores;
+    }
+}
+
---
+++ juan.letteri.3c/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public enum eColores
+        {
+            rojo,
+            azul,
+            verde,
+            amarillo,
+            violeta
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            cmb.Items.Add("hola");
+            cmb.Items.Add("chau");
+
+            foreach (eColores item in Enum.GetValues(typeof(eColores )))
+            {
+                cmbColores.Items.Add(item);
+            }
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            lblIndex.Text = cmb.SelectedIndex.ToString();
+            lblText.Text = cmb.SelectedText.ToString();
+            lblItem.Text= cmb.SelectedItem.ToString();
+            /*try
+            {
+            lblValue.Text= cmb.SelectedValue.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }*/
+
+        }
+
+        private void cmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnAceptar_Click(sender, e);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Seguro desera salir?","Saliendo...", MessageBoxButtons.OKCancel,MessageBoxIcon.Information) == DialogResult.OK)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+    }
+}
